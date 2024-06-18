af3dc205a546af572f2919c36c0b1a634fd2ad2b
TaiseiYokoshima <taiseiyokoshima030712@gmail.com>
2024-06-18 11:37:25 +0900

    lsp support and autocompletion for sql and bash

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,8 @@ return {
 					"jsonls",
 					"clangd",
 					"taplo",
+					"bashls",
+					"sqlls",
 					--          "java-language-server"
 				},
 				automatic_installation = true,
@@ -29,43 +31,46 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local on_attach = function(client, bufnr)
-        local opts = { buffer = bufnr, remap = false }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-      end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			local on_attach = function(client, bufnr)
+				local opts = { buffer = bufnr, remap = false }
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
+				vim.keymap.set("n", "<leader>vws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+				vim.keymap.set("n", "<leader>ca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				vim.keymap.set("n", "<leader>rr", function()
+					vim.lsp.buf.references()
+				end, opts)
+				vim.keymap.set("n", "<leader>rn", function()
+					vim.lsp.buf.rename()
+				end, opts)
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+			end
 
 			local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({ capabilities = capabilities, on_attach = on_attach})
+			lspconfig.lua_ls.setup({ capabilities = capabilities, on_attach = on_attach })
 			lspconfig.rust_analyzer.setup({
 				capabilities = capabilities,
-        on_attach = on_attach,
+				on_attach = on_attach,
 				--        on_attach = require("plugins.configs.lspconfig").on_attach,
 				filetypes = { "rust" },
 				root_dir = lspconfig.util.root_pattern("Cargo.toml"),
@@ -92,19 +97,23 @@ return {
 				single_file_support = true,
 			})
 
-			lspconfig.pyright.setup({ capabilities = capabilities, on_attach = on_attach})
+			lspconfig.pyright.setup({ capabilities = capabilities, on_attach = on_attach })
 
 			--      local capabilities2 = vim.lsp.protocol.make_client_capabilities()
 			--      capabilities2.textDocument.completionItem.snippetSupport = true
 			lspconfig.jsonls.setup({
 				capabilities = capabilities,
-        on_attach = on_attach
+				on_attach = on_attach,
 			})
 
 			lspconfig.clangd.setup({ capabilities = capabilities, on_attach = on_attach })
 
 			lspconfig.taplo.setup({ capabilities = capabilities, on_attach = on_attach })
-
+			lspconfig.bashls.setup({ capabilities = capabilities, on_attach = on_attach })
+			lspconfig.sqlls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach
+			})
 		end,
 	},
 
