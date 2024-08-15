18a92372ab5abfc89a8c7d47fee3c24d58e32ad7
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2024-08-15 22:39:58 +0900

    added comparators to fix ordering as well as keybind

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -23,40 +23,76 @@ return {
             require("luasnip").lsp_expand(args.body)
           end,
         },
+
+
         window = {
+          -- completion = {
+          --
+          -- },
+
+
+
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
+
+
+
+        sorting = {
+          comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.kind,
+          }
+        },
+
+
+
         mapping = cmp.mapping.preset.insert({
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-m>"] = cmp.mapping.scroll_docs(4),
+
+
+
+
           ["<C-a>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<C-f>"] = cmp.mapping.confirm({ select = true }),
+
           ["<Up>"] = cmp.mapping(function(fallback)
             cmp.close()
             fallback()
           end, { "i", "c" }),
+
           ["<Down>"] = cmp.mapping(function(fallback)
             cmp.close()
             fallback()
           end, { "i", "c" }),
+
           ["<Left>"] = cmp.mapping(function(fallback)
             cmp.close()
             fallback()
           end, { "i", "c" }),
+
           ["<Right>"] = cmp.mapping(function(fallback)
             cmp.close()
             fallback()
           end, { "i", "c" }),
         }),
+
         preselect = cmp.PreselectMode.Item,
 
         sources = cmp.config.sources({
+
           { name = "nvim_lsp" },
           { name = "luasnip" },
+          { name = "path" },
+
         }, {
           { name = "buffer" },
         }),
