93ee92b8291784a56e4f4369bb19bc45236d7bdc
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2024-09-06 18:20:34 +0900

    fixed depcreated function call

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -120,7 +120,7 @@ return {
         },
       })
 
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
         init_options = {
